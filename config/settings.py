65398f623a0f139d2889412212f10741816650ad
Bayrambek <bmbayrambek1600@gmail.com>
2024-12-13 17:13:02 +0500

    [add] final step

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$wmizrm4qs4ifrf@ep_c%9te8o1)s9f5sjo9&a6*=bd%dfiaqi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = "user.CustomUser"
 
 # Application definition
